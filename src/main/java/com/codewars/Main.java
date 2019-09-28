1d7a81647240b68b8b72cb972e46551e3aedec54
MJKosinski <kosinski.geodezja@gmail.com>
2019-09-28 21:45:08 +0200

    SimpleAssembler - done

--- src/main/java/com/codewars/Main.java
+++ src/main/java/com/codewars/Main.java
@@ -1,13 +1,20 @@
 package com.codewars;
 
+import sun.awt.image.IntegerComponentRaster;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        Map<String, Integer> testMap = new HashMap<>();
+        testMap.put("a",12);
+        System.out.println(testMap.get("a"));
+        testMap.replace("a",13);
+        System.out.println(testMap.get("a"));
 
 
-        System.out.println(gcd(10,41));
     }
 
-    static long gcd(long a, long b) {
-        return b == 0 ? a : gcd(b, a % b);
-    }
+
 }
--- src/main/java/com/codewars/SimpleAssembler.java
+++ src/main/java/com/codewars/SimpleAssembler.java
@@ -1,10 +1,56 @@
 package com.codewars;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class SimpleAssembler {
     public static Map<String, Integer> interpret(String[] program){
-        return null;
+
+        Map<String , Integer> result = new HashMap<>();
+
+        for (int i = 0; i < program.length; i++) {
+            String[] splited = program[i].split(" ");
+            switch (splited[0]) {
+                case "mov": {
+                    try {
+                        result.put(splited[1], Integer.valueOf(splited[2]));
+                    }
+                    catch(NumberFormatException e) {
+                        result.put(splited[1], result.get(splited[2]));
+                    }
+                    break;
+                }
+                case "inc": {
+                result.replace(splited[1],result.get(splited[1])+1);
+                break;
+                }
+
+                case "dec": {
+                    result.replace(splited[1],result.get(splited[1])-1);
+                    break;
+                }
+
+                case "jnz": {
+                    int x;
+                    try {
+                        x = Integer.parseInt(splited[1]);
+                    }
+                    catch (NumberFormatException e) {
+                        x = result.get(splited[1]);
+                    }
+
+                    if(x != 0) {
+                        i = i + Integer.parseInt(splited[2])-1;
+                    }
+                    break;
+                }
+            }
+
+
+        }
+
+
+        return result;
     }
 }
 
@@ -44,4 +90,50 @@ and then increase its value by 1, leaving register a at 1
 So, the function should return
 
 {a=1}
+
+
+
+java.lang.NullPointerException
+	at SimpleAssembler.interpret(SimpleAssembler.java:32)
+	at SolutionTest.complex_1(SolutionTest.java:35)
+	at java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+	at java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
+	at java.base/jdk.internal.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
+	at java.base/java.lang.reflect.Method.invoke(Method.java:566)
+	at org.junit.runners.model.FrameworkMethod$1.runReflectiveCall(FrameworkMethod.java:50)
+	at org.junit.internal.runners.model.ReflectiveCallable.run(ReflectiveCallable.java:12)
+	at org.junit.runners.model.FrameworkMethod.invokeExplosively(FrameworkMethod.java:47)
+	at org.junit.internal.runners.statements.InvokeMethod.evaluate(InvokeMethod.java:17)
+	at org.junit.runners.ParentRunner.runLeaf(ParentRunner.java:325)
+	at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:78)
+	at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:57)
+	at org.junit.runners.ParentRunner$3.run(ParentRunner.java:290)
+	at org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:71)
+	at org.junit.runners.ParentRunner.runChildren(ParentRunner.java:288)
+	at org.junit.runners.ParentRunner.access$000(ParentRunner.java:58)
+	at org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:268)
+	at org.junit.runners.ParentRunner.run(ParentRunner.java:363)
+	at org.junit.runner.JUnitCore.run(JUnitCore.java:137)
+	at org.junit.runner.JUnitCore.run(JUnitCore.java:115)
+	at org.junit.vintage.engine.execution.RunnerExecutor.execute(RunnerExecutor.java:40)
+	at java.base/java.util.stream.ForEachOps$ForEachOp$OfRef.accept(ForEachOps.java:183)
+	at java.base/java.util.stream.ReferencePipeline$3$1.accept(ReferencePipeline.java:195)
+	at java.base/java.util.Iterator.forEachRemaining(Iterator.java:133)
+	at java.base/java.util.Spliterators$IteratorSpliterator.forEachRemaining(Spliterators.java:1801)
+	at java.base/java.util.stream.AbstractPipeline.copyInto(AbstractPipeline.java:484)
+	at java.base/java.util.stream.AbstractPipeline.wrapAndCopyInto(AbstractPipeline.java:474)
+	at java.base/java.util.stream.ForEachOps$ForEachOp.evaluateSequential(ForEachOps.java:150)
+	at java.base/java.util.stream.ForEachOps$ForEachOp$OfRef.evaluateSequential(ForEachOps.java:173)
+	at java.base/java.util.stream.AbstractPipeline.evaluate(AbstractPipeline.java:234)
+	at java.base/java.util.stream.ReferencePipeline.forEach(ReferencePipeline.java:497)
+	at org.junit.vintage.engine.VintageTestEngine.executeAllChildren(VintageTestEngine.java:80)
+	at org.junit.vintage.engine.VintageTestEngine.execute(VintageTestEngine.java:71)
+	at org.junit.platform.launcher.core.DefaultLauncher.execute(DefaultLauncher.java:229)
+	at org.junit.platform.launcher.core.DefaultLauncher.lambda$execute$6(DefaultLauncher.java:197)
+	at org.junit.platform.launcher.core.DefaultLauncher.withInterceptedStreams(DefaultLauncher.java:211)
+	at org.junit.platform.launcher.core.DefaultLauncher.execute(DefaultLauncher.java:191)
+	at org.junit.platform.launcher.core.DefaultLauncher.execute(DefaultLauncher.java:128)
+	at io.qualified.junit5.RunTests.main(RunTests.java:25)
+
+
  */
