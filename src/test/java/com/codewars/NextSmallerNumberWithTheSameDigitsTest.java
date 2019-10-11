c2957193d078bb56603f1085a5532e0e31ea7746
MJKosinski <kosinski.geodezja@gmail.com>
2019-10-11 17:34:13 +0200

    Next smaller number with the same digits - empty

---
+++ src/test/java/com/codewars/NextSmallerNumberWithTheSameDigitsTest.java
@@ -0,0 +1,19 @@
+package com.codewars;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class NextSmallerNumberWithTheSameDigitsTest {
+
+    @Test
+    public void basicTests() {
+        assertEquals(12, NextSmallerNumberWithTheSameDigits.nextSmaller(21));
+        assertEquals(790, NextSmallerNumberWithTheSameDigits.nextSmaller(907));
+        assertEquals(513, NextSmallerNumberWithTheSameDigits.nextSmaller(531));
+        assertEquals(-1, NextSmallerNumberWithTheSameDigits.nextSmaller(1027));
+        assertEquals(414, NextSmallerNumberWithTheSameDigits.nextSmaller(441));
+        assertEquals(123456789, NextSmallerNumberWithTheSameDigits.nextSmaller(123456798));
+    }
+
+}
