f272290ede8bc04860857c805d050da3d19798f2
MJKosinski <kosinski.geodezja@gmail.com>
2020-01-02 19:12:52 +0100

    EsolangInterpreters - in progress

--- src/main/java/com/codewars/Main.java
+++ src/main/java/com/codewars/Main.java
@@ -7,13 +7,7 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-//        Map<String, Integer> testMap = new HashMap<>();
-//        testMap.put("a",12);
-//        System.out.println(testMap.get("a"));
-//        testMap.replace("a",13);
-//        System.out.println(testMap.get("a"));
-        int i = "3".compareTo("4");
-        System.out.println(i);
+
 
 
     }
